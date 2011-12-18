e5ddf36327f071b919fe03230284ea9383726c39
dilip <dilipchouhan.mca2006@gmail.com>
2011-12-18 20:23:01 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highrise::Application.config.secret_token = '23a0b0aac851e616dedec5010705bcb480740a4fc9805f22e280b581764ae5ed81566fee1b697ba080974ffe31a4fd94374176f2fd0bc9518a2eb7b1d696a8e2'
